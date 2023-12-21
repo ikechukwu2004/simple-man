ed6d395eab045bf977a1300aec2233082739d701
ikechukwu2004 <154479239+ikechukwu2004@users.noreply.github.com>
2023-12-21 01:22:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function red(){
+    document.getElementById('red').style.backgroundColor= "red";
+}
+
+function yellow(){
+    document.getElementById('yellow').style.backgroundColor= "yellow";
+}
+
+function green(){
+    document.getElementById('green').style.backgroundColor= "green";
+}
